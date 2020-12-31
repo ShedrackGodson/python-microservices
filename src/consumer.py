635c050975aef3df79ac42a521219a26a3dbb071
ShedrackGodson <hedrickgodson23@gmail.com>
2020-12-31 19:16:21 +0300

    automatic acknoledgement for consumer

--- src/consumer.py
+++ src/consumer.py
@@ -14,7 +14,7 @@ def callback(ch, method, properties, body):
     print(body)
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback)
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 
 print("Started consuming")
 
